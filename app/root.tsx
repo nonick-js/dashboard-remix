62344c667bad338649be4ddc409c310493cd0b3d
nonick <87268971+nonick-mc@users.noreply.github.com>
2024-06-27 10:10:29 +0900

    NextUI、Biome.jsの設定

--- app/root.tsx
+++ app/root.tsx
@@ -1,30 +1,34 @@
-import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
-import "./tailwind.css";
+import { Links, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
+import './tailwind.css';
+import { NextUIProvider } from '@nextui-org/react';
+import { ThemeProvider } from 'next-themes';
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang='ja'>
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
         <Meta />
         <Links />
       </head>
       <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
+        <NextUIProvider>
+          <ThemeProvider attribute='class' defaultTheme='dark'>
+            {children}
+            <ScrollRestoration />
+            <Scripts />
+          </ThemeProvider>
+        </NextUIProvider>
       </body>
     </html>
   );
 }
 
 export default function App() {
-  return <Outlet />;
+  return (
+    <main>
+      <Outlet />
+    </main>
+  );
 }
---
+++ app/routes/about.tsx
@@ -0,0 +1,5 @@
+import { Button } from '@nextui-org/react';
+
+export default function About() {
+  return <Button>About</Button>;
+}
